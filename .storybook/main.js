c4ac97e42a41ea353a95ce9ac89b2d5818852051
Diego <diego.souza@ciapetro.com.br>
2020-02-28 15:17:35 -0300

    taskbox UI Fim

--- .storybook/main.js
+++ .storybook/main.js
@@ -6,7 +6,12 @@
 //     '@storybook/addon-links',
 //   ],
 // };
+// module.exports = {
+//   stories: ['../src/components/**/*.stories.js'],
+//   addons: ['@storybook/addon-actions', '@storybook/addon-links'],
+// };
+
 module.exports = {
   stories: ['../src/components/**/*.stories.js'],
-  addons: ['@storybook/addon-actions', '@storybook/addon-links'],
+  addons: ['@storybook/addon-actions', '@storybook/addon-knobs', '@storybook/addon-links'],
 };
